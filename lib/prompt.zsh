4d171e0bf38eb74a79c9d52af41aedde9d4c6f76
TurchenkoAlex <turchenko.alexander@gmail.com>
2012-09-23 21:09:06 +0400

    Color for rprompt

--- lib/prompt.zsh
+++ lib/prompt.zsh
@@ -1,9 +1,11 @@
 require_library 'spectrum'
 
 local pscolor="green"
+local rpscolor="236"
 if [ $UID -eq 0 ] ; then
 	pscolor="red"
+	rpscolor="052"
 fi
 
 PROMPT="%{$fg_bold[$pscolor]%}[%{$fg_no_bold[$pscolor]%}%~%{$fg_bold[$pscolor]%}] %{$reset_color%}"
-RPROMPT="$FG[234]%n@%m%{$reset_color%}%"
+RPROMPT="$FG[$rpscolor]%n@%m%{$reset_color%}%"
