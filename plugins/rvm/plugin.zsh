6ddde18da02a5110e4c3f7b02bb0e73c78eee3a0
TurchenkoAlex <turchenko.alexander@gmail.com>
2012-09-24 17:05:11 +0400

    rvm plugin fix

--- plugins/rvm/plugin.zsh
+++ plugins/rvm/plugin.zsh
@@ -3,7 +3,7 @@ if [[ -d $HOME/.rvm ]]; then
 	[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
 else
 	if [[ -d /usr/local/rvm ]]; then
-		export PATH=$PATH:$HOME/usr/local/rvm/bin
+		export PATH=$PATH:/usr/local/rvm/bin
 		[[ -s "/usr/local/rvm/scripts/rvm" ]] && . "/usr/local/rvm/scripts/rvm"
 	fi
 fi
