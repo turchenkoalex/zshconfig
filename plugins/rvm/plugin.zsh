37179bf98980cb2baa2395c0d6a617a55cedcbd0
TurchenkoAlex <turchenko.alexander@gmail.com>
2012-11-12 21:13:20 +0400

    rvm plugin completion added
    Add rvm completion file path to fpath variable with dynamic rvm path
    eval

--- plugins/rvm/plugin.zsh
+++ plugins/rvm/plugin.zsh
@@ -1,9 +1,11 @@
 if [[ -d $HOME/.rvm ]]; then
 	export PATH=$PATH:$HOME/.rvm/bin
 	[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm"
+	fpath=($HOME/.rvm/scripts/zsh/Completion $fpath)
 else
 	if [[ -d /usr/local/rvm ]]; then
 		export PATH=$PATH:/usr/local/rvm/bin
 		[[ -s "/usr/local/rvm/scripts/rvm" ]] && . "/usr/local/rvm/scripts/rvm"
+		fpath=(/usr/local/rvm/scripts/zsh/Completion $fpath)
 	fi
 fi
