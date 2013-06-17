fa718c90732baaaf83ee529d8007d50a09e01549
TurchenkoAlex <turchenko.alexander@gmail.com>
2013-06-17 23:08:04 +0400

    Change color for right prompt for more lighter

--- lib/prompt.zsh
+++ lib/prompt.zsh
@@ -1,10 +1,10 @@
 require_library 'spectrum'
 
 local pscolor="green"
-local rpscolor="236"
+local rpscolor="238"
 if [ $UID -eq 0 ] ; then
 	pscolor="red"
-	rpscolor="052"
+	rpscolor="203"
 fi
 
 PROMPT="%{$fg_bold[$pscolor]%}[%{$fg_no_bold[$pscolor]%}%~%{$fg_bold[$pscolor]%}] %{$reset_color%}"
