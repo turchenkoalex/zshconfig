059a1ae4f347cf152613854b4bd55ba2ad49acb3
Turchenko Alexander <turchenko.alexander@gmail.com>
2013-11-15 21:12:52 +0400

    Plugin tag implemented

--- plugins/tag/plugin.zsh
+++ plugins/tag/plugin.zsh
@@ -1,13 +1,26 @@
-[[ "$zshconfig_tag_file" == "" ]] && zshconfig_tag_file=$ZSHCONFIG_DATA/.zshconfig_tags
+[[ "$zshconfig_plugin_tag_path" == "" ]] && zshconfig_plugin_tag_path=$ZSHCONFIG_DATA/tags
 
 function tag {
-	local tag_name=$1
-	local dir_path=$2
-	[[ "$dir_path" == "" ]] && dir_path=`pwd`
-	echo "$tag_name=$dir_path" >> $zshconfig_tag_file
-	eval "alias cd$tag_name=$dir_path"
+  local tag_name=$1
+  local dir_path=$2
+  [[ "$dir_path" == "" ]] && dir_path=`pwd`
+  eval "export $tag_name=$dir_path"
+  mkdir -p $zshconfig_plugin_tag_path
+  echo export $tag_name=$dir_path > $zshconfig_plugin_tag_path/$tag_name.tag
 }
 
 function untag {
-	unalias $1
+  local tag_name=$1
+  eval "unset $tag_name"
+  rm -f $zshconfig_plugin_tag_path/$tag_name.tag
 }
+
+function zshconfig_plugin_tag_load() {
+  if test -n "$(find $zshconfig_plugin_tag_path -maxdepth 1 -name '*.tag' -print -quit)"; then
+    source $zshconfig_plugin_tag_path/*.tag
+  fi
+}
+
+zshconfig_plugin_tag_load
+unset -f zshconfig_plugin_tag_load
+
